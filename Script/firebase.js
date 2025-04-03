1cf0a270fd26d2eb8a7e3bda3c6ac1634898b9b4
Ridhat-2000 <ridhat2000@gmail.com>
2025-04-03 17:30:27 +0530

    Add files via upload

---
+++ Script/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+
+const firebaseConfig = {
+apiKey: "AIzaSyCWX43Nv4visCEJEZ4OuXRFsvyC9s2DNdA",
+authDomain: "share-3486f.firebaseapp.com",
+projectId: "share-3486f",
+storageBucket: "share-3486f.firebasestorage.app",
+messagingSenderId: "931606779224",
+appId: "1:931606779224:web:1b3900dc12f8491965c12d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
